8ab5bc94e3738b8f1539b5bbc9fda2cebb5cef57
Kenver <kendcunha@gmail.com>
2025-07-06 14:12:48 +0300

    Fix API route to use mongoose

--- app/api/test-mongo/route.js
+++ app/api/test-mongo/route.js
@@ -1,8 +1,10 @@
-import { NextResponse } from "next/server";
-import { connectToDB } from "../../../lib/mongodb";
+import { connectToDB } from "../../../lib/mongoose";
 
 export async function GET() {
-  const db = await connectToDB();
-  const collections = await db.listCollections().toArray();
-  return NextResponse.json({ collections });
+  try {
+    await connectToDB();
+    return new Response(JSON.stringify({ success: true, message: "Connected to MongoDB!" }), { status: 200 });
+  } catch (err) {
+    return new Response(JSON.stringify({ success: false, error: err.message }), { status: 500 });
+  }
 }
